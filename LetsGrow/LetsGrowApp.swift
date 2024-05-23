6a5d2972df9896beb31fa102c7db34f9dd68fcd7
albhandari <alexbhandari37@gmail.com>
2024-05-22 17:42:38 -0700

    initial commit

---
+++ LetsGrow/LetsGrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsGrowApp.swift
+//  LetsGrow
+//
+//  Created by Alex Bhandari on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LetsGrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
