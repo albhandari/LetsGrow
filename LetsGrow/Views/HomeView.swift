6510c89d0af109e389402da36c0896fab887169a
albhandari <alexbhandari37@gmail.com>
2024-07-11 12:12:11 -0700

    Setup for tabbar

---
+++ LetsGrow/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  LetsGrow
+//
+//  Created by Alex Bhandari on 7/11/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        TabView{
+            Text("Home")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
