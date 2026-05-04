b808f106b6fb9ffaa731506406381d160a565b53
alexb <alexbhandari37@gmail.com>
2026-05-04 13:02:44 -0700

    Initial Commit

---
+++ LetsGrow/LetsGrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsGrowApp.swift
+//  LetsGrow
+//
+//  Created by Alex on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LetsGrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LetsGrowTests/LetsGrowTests.swift
@@ -0,0 +1,16 @@
+//
+//  LetsGrowTests.swift
+//  LetsGrowTests
+//
+//  Created by Alex on 5/4/26.
+//
+
+import Testing
+
+struct LetsGrowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
