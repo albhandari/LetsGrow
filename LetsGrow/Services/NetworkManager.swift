05cd9a85a5f0297aeb4f9948ee255bb484bb6b67
alexb <alexbhandari37@gmail.com>
2026-05-04 17:35:06 -0700

    Incorporate basic networking Manager service for decodable data

---
+++ LetsGrow/Services/NetworkManager.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+//Network Errors
+enum NetworkError: Error {
+    case invalidURL
+    case badResponse(statusCode: Int)
+    case decodingError(Error)
+    case requestFailed(Error)
+}
+
+protocol NetworkServiceProtocol {
+    func fetch<T: Decodable>(request: URLRequest) async throws -> T
+}
+
+final class NetworkManager: NetworkServiceProtocol {
+    // A singleton is okay here, but we will inject it as a protocol anyway
+    static let shared = NetworkManager()
+    
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func fetch<T: Decodable>(request: URLRequest) async throws -> T {
+        do {
+            let (data, response) = try await session.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw NetworkError.badResponse(statusCode: 0)
+            }
+            
+            guard (200...299).contains(httpResponse.statusCode) else {
+                throw NetworkError.badResponse(statusCode: httpResponse.statusCode)
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                return decodedData
+            } catch {
+                throw NetworkError.decodingError(error)
+            }
+        } catch let error as NetworkError {
+            throw error // Re-throw custom errors
+        } catch {
+            throw NetworkError.requestFailed(error)
+        }
+    }
+}
