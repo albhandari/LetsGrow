557a6ecfa85b8c873fea4a61322f363987b93fdb
alexb <alexbhandari37@gmail.com>
2026-05-07 13:22:50 -0700

    Boiler Plate views for Main tab View

---
+++ LetsGrow/Views/ShopView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct ShopView: View {
+    var body: some View {
+        NavigationStack{
+            Text("Shop: Rewards Store goes here")
+                .navigationTitle("Reward")
+        }
+    }
+}
---
+++ LetsGrow/Views/TimerView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        NavigationStack{
+            Text("Timer: Timer Functionality goes here")
+                .navigationTitle("Focus")
+        }
+    }
+}
